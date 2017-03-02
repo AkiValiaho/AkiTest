9dca501e9cb390db502117a6159d1dc60d83012c
Aki Väliaho <akivvaliaho@gmail.com>
2017-03-02 22:52:53 +0000

    Added no noargs constructor object initialization

--- project/AkiTestingClientCode/src/main/java/com/akivaliaho/AkiTestingClientCode/BeerService.java
+++ project/AkiTestingClientCode/src/main/java/com/akivaliaho/AkiTestingClientCode/BeerService.java
@@ -4,4 +4,17 @@ package com.akivaliaho.AkiTestingClientCode;
  * Created by vagrant on 3/1/17.
  */
 public class BeerService {
+    public String noArgsString;
+
+    public BeerService(String noArgsString) {
+        this.noArgsString = noArgsString;
+    }
+
+    public String getNoArgsString() {
+        return noArgsString;
+    }
+
+    public void setNoArgsString(String noArgsString) {
+        this.noArgsString = noArgsString;
+    }
 }
